665c370b78994f49f79ee5232babb2f46855258a
Fibonacci <dave.green1664@yahoo.co.uk>
2020-06-05 17:17:30 +0100

    Implemented my own simple sort algo.

--- Main.cpp
+++ Main.cpp
@@ -9,8 +9,10 @@ std::ofstream out("data.out");
 // Because this is global it is initialized with all elements to 0
 // This would not be the case if created locally in Main.
 int globArr[10];
+int numOfLoops;
 
 void printArr(const int(&myArr) [10], int arrSize);
+void sortArr(int(&myArr)[10], int arrSize);
 
 int main()
 {
@@ -23,7 +25,35 @@ int main()
 
 	//printArr(localArr, 10);
 
+	in >> numOfLoops;
 
+	/*for (int i = 0; i < numOfLoops; ++i)
+	{
+		int indexPos;
+		in >> indexPos;
+		++globArr[indexPos];
+	}
+
+	if (globArr[5] > 0)
+	{
+		out << "Yes, number in the array.\n";
+	}
+	else
+	{
+		out << "No, number is not in the array.\n";
+	}*/
+
+	for (int i = 0; i < numOfLoops; ++i)
+	{
+		int num;
+		in >> num;
+
+		globArr[i] = num;
+	}
+
+	//printArr(globArr, 10);
+	sortArr(globArr, 10);
+	//printArr(globArr, 10);
 
 	return 0;
 }
@@ -32,6 +62,27 @@ void printArr(const int(&myArr)[10], int arrSize)
 {
 	for (int i = 0; i < arrSize; ++i)
 	{
-		std::cout << "Element " << i << " = " << myArr[i] << '\n';
+		std::cout << "[" << myArr[i] << "],\t";
+	}
+
+	std::cout << '\n';
+	std::cout << '\n';
+}
+
+void sortArr(int(&myArr)[10], int arrSize)
+{
+	for (int i = 0; i < arrSize; ++i)
+	{
+		for (int j = i; j < arrSize; ++j)
+		{
+			if (myArr[j] < myArr[i])
+			{
+				int temp = myArr[i];
+				myArr[i] = myArr[j];
+				myArr[j] = temp;
+			}
+
+			printArr(myArr, 10);
+		}		
 	}
 }
